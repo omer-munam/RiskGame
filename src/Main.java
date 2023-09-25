301bc65db3836b76c55c66153df990c510846778
Omer Munam <omer_munam@yahoo.com>
2023-09-25 19:26:25 -0400

    Issue Orders Completed

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ import Models.WarMap;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+    public static Scanner SCANNER;
     public static void main(String[] args) throws IOException {
-
+        SCANNER = new Scanner(System.in);
     }
 }
