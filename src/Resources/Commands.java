586e3d3d87f762b3d39fb865d209e5dc05cbd20e
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2023-09-25 19:11:15 -0400

    All of the possible commands added

--- src/Resources/Commands.java
+++ src/Resources/Commands.java
@@ -1,6 +1,15 @@
 package Resources;
 
 public class Commands {
-
-    public static final String LOADMAP_COMMAND = "loadmap";
+    public static final String LOAD_MAP_COMMAND = "loadmap";
+    public static final String SHOW_MAP_COMMAND = "showmap";
+    public static final String SAVE_MAP_COMMAND = "savemap";
+    public static final String EDIT_MAP_COMMAND = "editmap";
+    public static final String VALIDATE_MAP_COMMAND = "validatemap";
+    public static final String ASSIGN_COUNTRIES_COMMAND = "assigncountries";
+    public static final String PLAYER_ADD_REMOVE_COMMAND = "gameplayer";
+    public static final String EDIT_CONTINENT_COMMAND = "editcontinent";
+    public static final String EDIT_COUNTRY_COMMAND = "editcountry";
+    public static final String EDIT_NEIGHBOR_COMMAND = "editneighbor";
+    public static final String DEPLOY_COMMAND = "deploy";
 }
