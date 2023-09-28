86c7c5e3acb9c18dee55c746d407d07c0ee52d0a
Omer Munam <omer_munam@yahoo.com>
2023-09-28 17:46:40 -0400

    Removed Unnecesary Scanner

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner SCANNER;
     public static void main(String[] args) throws IOException {
         GameEngine new_game = new GameEngine();
         new_game.start_game();
