c8955b22a1f41c54aa3cf5b3d2b12846a79b3371
Ryan Feher <47124415+NearRS@users.noreply.github.com>
2023-10-02 17:26:54 -0400

    Added getNumOfReinforcements

--- src/Controller/MainGameLoop.java
+++ src/Controller/MainGameLoop.java
@@ -1,8 +1,12 @@
 package Controller;
 
+import Models.Country;
 import Models.Player;
 import Models.WarMap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -39,5 +43,41 @@ public class MainGameLoop {
         System.out.println("Begin Main Game Loop...");
     }
 
+    void assign_reinforcements() {
+
+    }
+
+    int getNumOfReinforcements(Player p_player) {
+        int l_baseReinforcements = 5;
+        d_map.get_countries();
+        p_player.get_playerCountries();
+        HashMap<Integer, ArrayList<Integer>> l_continent_countries = new HashMap<>();
+        for (Country l_c : d_map.get_countries().values()) {
+            l_continent_countries.putIfAbsent(l_c.getContinentID(), new ArrayList<Integer>());
+            l_continent_countries.get(l_c.getContinentID()).add(l_c.get_countryID());
+        }
+
+
+        HashSet<Integer> l_full_continents = new HashSet<>();
+        ArrayList<Integer> l_player_country_ids = new ArrayList<>();
+        for (Country l_country : p_player.get_playerCountries()) {
+            l_player_country_ids.add(l_country.get_countryID());
+        }
+
+        for (ArrayList<Integer> l_c : l_continent_countries.values()) {
+            for (int l_i : l_c) {
+                if (l_player_country_ids.contains(l_i)) {
+                    l_full_continents.add(d_map.get_countries().get(l_i).getContinentID());
+                } else {
+                    l_full_continents.remove(d_map.get_countries().get(l_i).getContinentID());
+                    break;
+                }
+            }
+        }
+        for (int l_i : l_full_continents) {
+            l_baseReinforcements += d_map.get_continents().get(l_i).get_armyBonus();
+        }
+        return l_baseReinforcements;
+    }
 
 }
