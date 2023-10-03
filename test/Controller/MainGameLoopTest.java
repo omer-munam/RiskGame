b9049efc27598a6b0cf07bdfa94bb0b327e858e4
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2023-10-03 15:55:32 -0400

    completed the testGetNumOfReinforcements()

--- test/Controller/MainGameLoopTest.java
+++ test/Controller/MainGameLoopTest.java
@@ -1,56 +1,60 @@
-//package Controller;
-//
-//import Models.Country;
-//import Models.Player;
-//import Models.WarMap;
-//import org.junit.jupiter.api.BeforeEach;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.HashSet;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class MainGameLoopTest {
-//
-//    @BeforeEach
-//    void setUp() {
-//        Player player1 = new Player("Adeel"); // Create an instance of the Player class
-//
-//
-//        WarMap game_map = new WarMap()
-//
-//        HashMap<Integer, ArrayList<Integer>> countries = new HashMap<>();
-//        HashSet<Integer> continents = new HashSet<>();
-//
-//        // Populate countries and continents as needed for testing
-//        // You can create dummy data or use a mocking library like Mockito
-//    }
-//    @org.junit.jupiter.api.Test
-//    void testGetNumOfReinforcements() {
-//        // Add countries to the player's list of owned countries
-//        ArrayList<Country> playerCountries = new ArrayList<>();
-//        // Add countries to playerCountries as needed for testing
-//        MainGameLoo player1;
-//        player1.setPlayerCountries(playerCountries);
-//
-//        // Create and add continents to the map
-//        Continent continent1 = new Continent(1, "Continent1", 3); // Replace with actual data
-//        Continent continent2 = new Continent(2, "Continent2", 5); // Replace with actual data
-//        continents.put(continent1.getContinentID(), continent1);
-//        continents.put(continent2.getContinentID(), continent2);
-//
-//        // Assign countries to continents
-//        // Populate the 'countries' map with Country instances and assign them to continents
-//
-//        // Set the 'd_map' member variable of the player to the map containing continents and countries
-//        player1.setD_map(new Map(continents, countries)); // Replace with actual Map class and data
-//
-//        int numOfReinforcements = player1.getNumOfReinforcements(player);
-//
-//        // Define the expected number of reinforcements based on your test scenario
-//        int expectedReinforcements = 8; // Adjust this value based on your specific test case
-//
-//        assertEquals(expectedReinforcements, numOfReinforcements);
-//    }
-//}
+package Controller;
+
+import Models.Continent;
+import Models.Country;
+import Models.Player;
+import Models.WarMap;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainGameLoopTest {
+    @org.junit.jupiter.api.Test
+    void testGetNumOfReinforcements() {
+        WarMap test_war_map = new WarMap();
+        //CREATE THE CONTINENTS
+        test_war_map.addContinent(new Continent(1, "continent 1", 1));
+        test_war_map.addContinent(new Continent(2, "continent 2", 2));
+
+        //CREATE THE COUNTRIES
+        Country test_country_one = new Country(1, "Country 1", 1);
+        Country test_country_two = new Country(2, "Country 2", 1);
+        Country test_country_three = new Country(3, "Country 3", 1);
+        Country test_country_four = new Country(4, "Country 4", 2);
+        Country test_country_five = new Country(5, "Country 5", 2);
+
+        //ADD ALL Countries to map.
+        test_war_map.addCountry(test_country_one);
+        test_war_map.addCountry(test_country_two);
+        test_war_map.addCountry(test_country_three);
+        test_war_map.addCountry(test_country_four);
+        test_war_map.addCountry(test_country_five);
+
+        //Create player and add desired coutnries to player
+        Player test_player = new Player("TestPlayer");
+        ArrayList<Country> player_countries = new ArrayList<>();
+        player_countries.add(test_country_one);
+        player_countries.add(test_country_two);
+        player_countries.add(test_country_three);
+        player_countries.add(test_country_four);
+
+        //Added full continent 1 (countries 1-3) but only one country from continent 2, should get 6 reinforcements, 5+army bonus of 1
+        test_player.set_playerCountries(player_countries);
+
+        ArrayList<Player> test_player_list = new ArrayList<>();
+        test_player_list.add(test_player); //NEED TO PUT PLAYER IN LIST TO MAKE MainGameLoop Class
+
+        MainGameLoop test_mainGameLoop = new MainGameLoop(test_war_map, test_player_list); //CREATE MainGameLoop with the created map and player list
+
+        int numOfReinforcements = test_mainGameLoop.getNumOfReinforcements(test_player);
+
+        // Define the expected number of reinforcements based on your test scenario
+        int expectedReinforcements = 6; // Adjust this value based on your specific test case
+
+        assertEquals(expectedReinforcements, numOfReinforcements);
+    }
+}
