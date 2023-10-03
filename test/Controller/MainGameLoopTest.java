398f9a7a699783fa01b7087f938e8acc09c29fe1
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2023-10-03 02:27:30 -0400

    Update MainGameLoopTest.java

--- test/Controller/MainGameLoopTest.java
+++ test/Controller/MainGameLoopTest.java
@@ -1,9 +1,13 @@
 package Controller;
 
+import Models.Country;
 import Models.Player;
+import Models.WarMap;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +15,13 @@ class MainGameLoopTest {
 
     @BeforeEach
     void setUp() {
-        Player player = new Player(); // Create an instance of the Player class
-        HashMap<Object, Object> countries = new HashMap<>();
-        continents = new HashMap<>();
+        Player player1 = new Player("Adeel"); // Create an instance of the Player class
+
+
+        WarMap game_map = new WarMap()
+
+        HashMap<Integer, ArrayList<Integer>> countries = new HashMap<>();
+        HashSet<Integer> continents = new HashSet<>();
 
         // Populate countries and continents as needed for testing
         // You can create dummy data or use a mocking library like Mockito
@@ -23,7 +31,8 @@ class MainGameLoopTest {
         // Add countries to the player's list of owned countries
         ArrayList<Country> playerCountries = new ArrayList<>();
         // Add countries to playerCountries as needed for testing
-        player.setPlayerCountries(playerCountries);
+        MainGameLoo player1;
+        player1.setPlayerCountries(playerCountries);
 
         // Create and add continents to the map
         Continent continent1 = new Continent(1, "Continent1", 3); // Replace with actual data
@@ -35,9 +44,9 @@ class MainGameLoopTest {
         // Populate the 'countries' map with Country instances and assign them to continents
 
         // Set the 'd_map' member variable of the player to the map containing continents and countries
-        player.setD_map(new Map(continents, countries)); // Replace with actual Map class and data
+        player1.setD_map(new Map(continents, countries)); // Replace with actual Map class and data
 
-        int numOfReinforcements = player.getNumOfReinforcements(player);
+        int numOfReinforcements = player1.getNumOfReinforcements(player);
 
         // Define the expected number of reinforcements based on your test scenario
         int expectedReinforcements = 8; // Adjust this value based on your specific test case
