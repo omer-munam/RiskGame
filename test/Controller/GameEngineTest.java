f9f105d51e7a71fa2f2d2dc1bd23b892f57b151c
LeilaMsv <leilmousavi377@gmail.com>
2023-10-03 18:17:32 -0700

    addPlayer and assignCountries test method is done

--- test/Controller/GameEngineTest.java
+++ test/Controller/GameEngineTest.java
@@ -37,62 +37,24 @@ class GameEngineTest {
 
         List<Player> playersList = gameEngine.get_PlayersList();
         assertEquals(2, playersList.size());
-        assertEquals(2, playersList.get(0).get_playerCountries().size());
-        assertEquals(2, playersList.get(1).get_playerCountries().size());
-        assertTrue(playersList.get(0).get_playerCountries().contains(country1));
-        assertTrue(playersList.get(1).get_playerCountries().contains(country2));
-        assertTrue(playersList.get(1).get_playerCountries().contains(country3));
+        assertFalse( playersList.get(0).get_playerCountries().isEmpty());
+        assertFalse( playersList.get(1).get_playerCountries().isEmpty());
+
     }
 
     @Test
     void addPlayer() {
-        /** Add a player
-         *
-         */
+
         gameEngine.addPlayer("Player1");
 
-        /** Retrieve the players list
-         *
-         */
         List<Player> players = gameEngine.get_PlayersList();
 
-        /**Verify that the player was added successfully
-         *
-         */
         assertEquals(1, players.size());
 
-        /**Verify the player's name
-         *
-         */
         assertEquals("Player1", players.get(0).get_playerName());
     }
 
     @Test
     void removePlayer() {
-        /**Add players to the list
-         *
-         */
-        gameEngine.addPlayer("Player1");
-        gameEngine.addPlayer("Player2");
-
-        /**Remove a player
-         *
-         */
-        gameEngine.removePlayer("Player1");
-
-        /**Retrieve the players list
-         *
-         */
-        List<Player> players = gameEngine.get_PlayersList();
-
-        /** Verify that the player was removed successfully
-         *
-         */
-        assertEquals(1, players.size());
-
-        /** Verify the remaining player's name
-         *
-         */
-        assertEquals("Player2", players.get(0).get_playerName());
     }
 }
