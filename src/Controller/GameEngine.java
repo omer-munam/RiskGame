26939f56faaa048a0947cc443f222ad215bdd80d
Ryan Feher <47124415+NearRS@users.noreply.github.com>
2023-10-04 19:57:52 -0400

    Small coding standard change

--- src/Controller/GameEngine.java
+++ src/Controller/GameEngine.java
@@ -144,19 +144,19 @@ public class GameEngine {
                             {
                                 if (l_userInput.toLowerCase().startsWith(Commands.PLAYER_EDIT_COMMAND) && l_words.length >= 3)
                                 {
-                                    for (int i = 1; i < l_words.length; i++) {
-                                        if (l_words[i].equals("-add")) {
-                                            i++;
-                                            if (i < l_words.length) {
-                                                addPlayer(l_words[i]);
+                                    for (int l_i = 1; l_i < l_words.length; l_i++) {
+                                        if (l_words[l_i].equals("-add")) {
+                                            l_i++;
+                                            if (l_i < l_words.length) {
+                                                addPlayer(l_words[l_i]);
                                             } else {
                                                 System.out.println("Reached end of command while parsing");
                                             }
                                         }
-                                        if (l_words[i].equals("-remove")) {
-                                            i++;
-                                            if (i < l_words.length) {
-                                                removePlayer(l_words[i]);
+                                        if (l_words[l_i].equals("-remove")) {
+                                            l_i++;
+                                            if (l_i < l_words.length) {
+                                                removePlayer(l_words[l_i]);
                                             } else {
                                                 System.out.println("Reached end of command while parsing");
 
