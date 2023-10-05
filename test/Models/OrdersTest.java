8ade5b19921b0862a9bcd3167d84b41cb66de00a
shezin888 <shezins888@gmail.com>
2023-10-04 23:08:40 -0400

    javadoc for ordertest

--- test/Models/OrdersTest.java
+++ test/Models/OrdersTest.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+import org.junit.jupiter.api.TestInstance;
 
 /**
- * Tests for executing orders
+ * Tests cases for executing orders, it ensures that the Orders class functions correctly according to the specified test cases.
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrdersTest {
     private WarMap warmap;
     private Country country;
 
+    /**
+     * Sets up the test environment before each test case.
+     * It initializes the WarMap and Country objects.
+     */
     @BeforeEach
     void setUp() {
         warmap = new WarMap();
@@ -22,6 +26,10 @@ public class OrdersTest {
 
     }
 
+    /**
+     *  Tests the execution of orders by the Orders class.
+     *   It creates an Orders object, executes it, and then asserts that the expected result is achieved
+     */
     @Test
     void testExecute() {
         Orders order = new Orders(5, country.get_countryID());
