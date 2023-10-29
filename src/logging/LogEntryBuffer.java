5c2b1c9ef1469eb6f24834ce29bc8b2935638c44
mohduvas <mohduvas024@gmail.com>
2023-10-29 15:36:21 -0400

    Observer Pattern Logging
    Used Observer pattern to implement logging messages in the game.
    LogEntryBuffer and LogWriter are singleton classes, so are called intially in main and LogWriter is attached in LogEntryBuffer List of Observers in main.
    
    whenever we need to log.
    first take instance using getinstance of LogEntryBuffer and call writelog function of it by passing the string log message as parameter.
    
    you can view the log file in logs folder of project.

---
+++ src/logging/LogEntryBuffer.java
@@ -0,0 +1,35 @@
+package logging;
+
+/**
+ * This class contains utility functions for creating and editing maps to be used in the game. It is also the entry point to be used from main to access the map editor.
+ *
+ * @author Mohammad Uvas
+ */
+public class LogEntryBuffer extends Observable{
+	
+	private static LogEntryBuffer instance = new LogEntryBuffer();
+	
+	/**
+	* Make the constructor private so that this class cannot be instantiated
+	*/
+	private LogEntryBuffer(){}
+	
+	/**
+	* If the instance was not previously created, create it. Then return the instance
+	*/
+	public static LogEntryBuffer getInstance(){
+		if (instance == null)
+		instance = new LogEntryBuffer();
+		return instance;
+	}
+	
+	/**
+	 * takes logging message from the running project and calls notifyObservers for notifying every attached Observer.
+	 * 
+	 * @return none
+	 */
+	public void writeLog(String p_logMessage) {
+		notifyObservers(p_logMessage);
+	}
+
+}
---
+++ src/logging/Observable.java
@@ -0,0 +1,48 @@
+package logging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that implements the connection/disconnection mechanism between
+ * observers and observables (subject). It also implements the notification
+ * mechanism that the observable will trigger when its state changes.
+ * 
+ * @author Mohammad Uvas
+ */
+public class Observable {
+	
+	private List<Observer> observers = new ArrayList<Observer>();
+
+	/**
+	 * attach a view to the model.
+	 * 
+	 * @param p_o: view to be added to the list of observers to be notified.
+	 */
+	public void attach(Observer p_o) {
+		this.observers.add(p_o);
+	}
+
+	/**
+	 * detach a view from the model.
+	 * 
+	 * @param p_o: view to be removed from the list of observers.
+	 */
+	public void detach(Observer p_o) {
+		if (!observers.isEmpty()) {
+			observers.remove(p_o);
+		}
+	}
+
+	/**
+	 * Notify all the views attached to the model.
+	 * 
+	 * @param p_logString: string that contains the information to be observed.
+	 */
+	public void notifyObservers(String p_logString) {
+		for (Observer observer : observers) {
+			observer.update(p_logString);
+		}
+	}
+	
+}
