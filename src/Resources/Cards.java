e5c6b06bf51c1a5e4636e7048b26040d2f03898d
Mohammed Omer Munam <mohammed.omer@astera.com>
2023-11-05 01:14:39 -0500

    Method to randomly assign a card to player

--- src/Resources/Cards.java
+++ src/Resources/Cards.java
@@ -1,8 +1,29 @@
 package Resources;
 
+import Models.Player;
+
+import java.util.Random;
+
+/**
+ * Enum class containing all possible cards and the logic of assigning random cards.
+ *
+ * @author Omer Munam
+ */
 public enum Cards {
     Bomb,
     Blockade,
     Airlift,
-    Diplomacy
+    Diplomacy;
+
+    /**
+     * Assign a random card to player. Call this method whenever player acquires new territory.
+     *
+     * @param p_player
+     */
+    public void assignRandomCardToPlayer(Player p_player) {
+        Random random = new Random();
+        p_player.get_playerCards().add(values()[random.nextInt(values().length)]);
+    }
 }
+
+
