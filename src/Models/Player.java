c8651e8051588120b845e049338d007207930544
Mohammed Omer Munam <mohammed.omer@astera.com>
2023-11-06 00:48:40 -0500

    Remove bomb card if order issued

--- src/Models/Player.java
+++ src/Models/Player.java
@@ -232,7 +232,7 @@ public class Player {
     }
 
     private void bomb_issue_order(String[] commandTokens, WarMap d_map) {
-        boolean hasBombCard = d_playerCards.contains(Cards.Bomb);
+        boolean hasBombCard = d_playerCards.remove(Cards.Bomb);
         if (!hasBombCard){
             System.out.println("Player does not have Bomb card");
             return;
@@ -260,6 +260,7 @@ public class Player {
 
         BombOrder order = new BombOrder(destCountryID);
         d_playerOrders.add(order);
+        d_playerCards.remove(Cards.Bomb);
         System.out.println("Bomb order issued successfully.");
     }
 
