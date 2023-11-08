dcbd7018b5705e5fb49aaded8365af556b4d4413
shezin888 <shezins888@gmail.com>
2023-11-07 21:22:40 -0500

    latest BlockadeOrderTest

--- test/Models/Orders/BlockadeOrderTest.java
+++ test/Models/Orders/BlockadeOrderTest.java
@@ -43,13 +43,11 @@ public class BlockadeOrderTest {
     }
 
     /**
-     * Tests the execution of orders by blockade
-     * It calls the blockade order execute method, and checks the num of Armies,
-     * after execution the country is made null
-     * checks if the country is removed from player country list
+     * Tests number of armies the execution of orders by blockade
+     * It calls the blockade order execute method, and checks if the num of Armies are tripled
      */
     @Test
-    public void testExecuteBlockadeOrder() {
+    public void testExecuteBlockadeOrderNumOfArmies() {
         Country destCountry1 = new Country(1, "DestCountry1", 1);
         destCountry1.set_numOfArmies(5);
         destCountry1.setD_ownerPlayer(player);
@@ -67,14 +65,76 @@ public class BlockadeOrderTest {
         blockadeOrder1.execute(warMap);
 
         assertEquals(15, destCountry1.get_numOfArmies());
-        assertNull(destCountry1.getD_ownerPlayer());
-        assertEquals(1, player.get_playerCountries().size());
 
         BlockadeOrder blockadeOrder2 = new BlockadeOrder(2, player);
         blockadeOrder2.execute(warMap);
 
         assertEquals(12, destCountry2.get_numOfArmies());
+    }
+
+    /**
+     * Tests the country owned by players after execution of orders by blockade
+     * Checks the country ownership is made null
+     * Checks the destination country is removed from the player owned country list
+     */
+    @Test
+    public void testExecuteBlockadeOrderCountryRemoved() {
+        Country destCountry1 = new Country(1, "DestCountry1", 1);
+        destCountry1.set_numOfArmies(5);
+        destCountry1.setD_ownerPlayer(player);
+        countries.put(1, destCountry1);
+
+        Country destCountry2 = new Country(2, "DestCountry2", 2);
+        destCountry2.set_numOfArmies(4);
+        destCountry2.setD_ownerPlayer(player);
+        countries.put(2, destCountry2);
+
+        Country destCountry3 = new Country(3, "DestCountry3", 2);
+        destCountry3.set_numOfArmies(2);
+        destCountry3.setD_ownerPlayer(player);
+        countries.put(3, destCountry3);
+
+        warMap.set_countries((HashMap<Integer, Country>) countries);
+        player.set_playerCountries(new ArrayList<>(countries.values()));
+
+        BlockadeOrder blockadeOrder1 = new BlockadeOrder(1, player);
+        blockadeOrder1.execute(warMap);
+
+        assertNull(destCountry1.getD_ownerPlayer());
+        assertEquals(2, player.get_playerCountries().size());
+
+        BlockadeOrder blockadeOrder2 = new BlockadeOrder(2, player);
+        blockadeOrder2.execute(warMap);
+
         assertNull(destCountry2.getD_ownerPlayer());
+        assertEquals(1, player.get_playerCountries().size());
+
+        BlockadeOrder blockadeOrder3 = new BlockadeOrder(3, player);
+        blockadeOrder3.execute(warMap);
+
+        assertNull(destCountry3.getD_ownerPlayer());
         assertEquals(0, player.get_playerCountries().size());
     }
+
+
+    /**
+     * Tests the execution of orders by blockade for non-existing players
+     * Checks if a non-existing country is called on blockade, it doesnt make any difference
+     */
+    @Test
+    public void testExecuteBlockadeOrderNonExistingCountry() {
+        Country existingCountry = new Country(1, "ExistingCountry", 1);
+        countries.put(1, existingCountry);
+        warMap.set_countries((HashMap<Integer, Country>) countries);
+
+        BlockadeOrder blockadeOrder = new BlockadeOrder(2, player);
+        blockadeOrder.execute(warMap);
+
+        assertEquals(existingCountry, warMap.get_countries().get(1));
+
+        assertEquals(0, player.get_playerCountries().size());
+
+        assertNull(warMap.get_countries().get(2));
+    }
+
 }
