5020883d4cc1f1327d39cd238c141eaa6d09fbe3
shezin888 <shezins888@gmail.com>
2023-11-08 00:03:11 -0500

    issue blockade Test

--- test/Models/PlayerTest.java
+++ test/Models/PlayerTest.java
@@ -1,10 +1,10 @@
 package Models;
 
-
 import Controller.GameEngine;
 import Models.Orders.DeployOrder;
 import Models.Orders.Order;
 import Phases.AssignReinforcements;
+import Resources.Cards;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -24,14 +24,20 @@ public class PlayerTest {
      * The player instance required to run the test case.
      */
     private Player player;
-    private GameEngine gameEngine;
     /**
      * Initializing the player instance before each test.
      */
+    private GameEngine gameEngine;
+    /**
+     * The Instance of warmap.
+     */
+    private WarMap warMap;
+
     @BeforeEach
     public void setUp() {
         gameEngine = GameEngine.getInstance();
         player = new Player("John Doe");
+        warMap = new WarMap();
     }
 
     /**
@@ -156,6 +162,85 @@ public class PlayerTest {
 
         // Ensure that the player's available reinforcements remain unchanged
         assertEquals(Integer.valueOf(0), player.get_numOfReinforcements());
+    }
+
+    /**
+     * Checks if the orders are taken correctly with valid Input
+     */
+    @Test
+    public void testBlockadeIssueOrderValidInput() {
+        Country country1 = new Country(1, "Country1", 1);
+        player.get_playerCountries().add(country1);
+        player.get_playerCards().add(Cards.Blockade);
+
+        Country country2 = new Country(2, "Country2", 1);
+        player.get_playerCountries().add(country2);
+        player.get_playerCards().add(Cards.Blockade);
+
+        String[] commandTokens1 = {"blockade", "1"};
+        player.blockade_issue_order(commandTokens1, warMap);
+
+        assertEquals(1, player.get_playerOrder().size());
+        assertTrue(player.get_playerOrder().get(0) != null);
+        assertEquals(1,player.get_playerCards().size());
+
+        String[] commandTokens2 = {"blockade", "2"};
+        player.blockade_issue_order(commandTokens2, warMap);
+
+        assertEquals(2, player.get_playerOrder().size());
+        assertTrue(player.get_playerOrder().get(1) != null);
+        assertFalse(player.get_playerCards().contains(Cards.Blockade));
+        assertEquals(0,player.get_playerCards().size());
+    }
+
+    /**
+     * Checks if no orders are added to orderlist and
+     * Card is not removed with Invalid input
+     */
+    @Test
+    public void testBlockadeIssueOrderInvalidCountry() {
+        // Prepare player's countries and cards
+        player.get_playerCards().add(Cards.Blockade);
+
+        String[] commandTokens1 = {"blockade", "2"};
+        player.blockade_issue_order(commandTokens1, warMap);
+
+        assertEquals(0, player.get_playerOrder().size());
+        assertTrue(player.get_playerCards().contains(Cards.Blockade));
+        assertEquals(1,player.get_playerCards().size());
+
+        player.get_playerCards().add(Cards.Blockade);
+        String[] commandTokens2 = {"blockade", "3"};
+        player.blockade_issue_order(commandTokens2, warMap);
+
+        assertEquals(0, player.get_playerOrder().size());
+        assertTrue(player.get_playerCards().contains(Cards.Blockade));
+        assertEquals(2,player.get_playerCards().size());
+    }
+
+    /**
+     * Checks if no orders are added to list
+     * if there is no blockade card in the player card list
+     */
+    @Test
+    public void testBlockadeIssueOrderNoBlockadeCard() {
+        Country country1 = new Country(1, "Country1", 1);
+        player.get_playerCountries().add(country1);
+
+        Country country2 = new Country(2, "Country2", 1);
+        player.get_playerCountries().add(country2);
+
+        String[] commandTokens1 = {"blockade", "1"};
+        player.blockade_issue_order(commandTokens1, warMap);
+
+        assertEquals(0, player.get_playerOrder().size());
+        assertFalse(player.get_playerCards().contains(Cards.Blockade));
+
+        String[] commandTokens2 = {"blockade", "2"};
+        player.blockade_issue_order(commandTokens2, warMap);
+
+        assertEquals(0, player.get_playerOrder().size());
+        assertFalse(player.get_playerCards().contains(Cards.Blockade));
     }
 }
 
