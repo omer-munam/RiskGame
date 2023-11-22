18935d9cc292b8e4b6c14a95b1ac3fe55417896d
shezin888 <shezins888@gmail.com>
2023-11-22 00:35:09 -0500

    initial behaviour class

---
+++ src/Resources/behaviors.java
@@ -0,0 +1,16 @@
+package Resources;
+
+
+
+/**
+ * Enum class containing all possible behaviors.
+ *
+ * @author Shezin Saleem
+ */
+public enum behaviors {
+    Human,
+    Aggressive,
+    Benevolent,
+    Random,
+    Cheater;
+}
