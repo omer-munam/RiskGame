661e4dd8afc580ef1ac94a3de9844dc7bba5951f
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2023-11-22 22:10:32 -0500

    BehaviourStrategy structure

---
+++ src/Models/BehaviourStrategies/BehaviourStrategy.java
@@ -0,0 +1,4 @@
+package Models.BehaviourStrategies;
+
+public interface BehaviourStrategy {
+}
---
+++ src/Models/BehaviourStrategies/CheaterStrategy.java
@@ -0,0 +1,4 @@
+package Models.BehaviourStrategies;
+
+public class CheaterStrategy implements BehaviourStrategy {
+}
