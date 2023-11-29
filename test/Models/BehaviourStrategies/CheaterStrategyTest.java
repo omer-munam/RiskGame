910201203fb6f452e88c82e50a3a5be353ad1512
LeilaMsv <leilmousavi377@gmail.com>
2023-11-29 01:10:59 -0800

    CheaterStrategyTest: test case added

--- test/Models/BehaviourStrategies/CheaterStrategyTest.java
+++ test/Models/BehaviourStrategies/CheaterStrategyTest.java
@@ -17,4 +17,28 @@ public class CheaterStrategyTest {
         cheaterStrategy = new CheaterStrategy(player);
     }
 
+    @Test
+    public void testIssueOrder_ConquerEnemyAndDoubleArmies() {
+        // Create a country with an enemy neighbor
+        Country country = new Country(1, "CountryA", 1);
+        Country enemyNeighbor = new Country(2, "CountryB", 1);
+        enemyNeighbor.setD_ownerPlayer(new Player("EnemyPlayer"));
+        country.addNeighbouringCountry(enemyNeighbor);
+        player.get_playerCountries().add(country);
+
+        // Set up initial armies
+        country.set_numOfArmies(6);
+        enemyNeighbor.set_numOfArmies(2);
+
+        // Issue order for the cheater strategy
+        cheaterStrategy.issue_order();
+
+        // Check if the enemy neighbor is conquered and armies are doubled
+        assertEquals(player, enemyNeighbor.getD_ownerPlayer());
+        assertEquals(4, enemyNeighbor.get_numOfArmies());
+
+        // Check if the armies on the original country are doubled
+        assertEquals(6, country.get_numOfArmies());
+    }
+
 }
