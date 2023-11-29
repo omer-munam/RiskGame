29c481136e87017862b50fbe80134f92b8ba11fb
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2023-11-29 16:21:51 -0500

    javadoc added for BenevolentStrategyTest

--- test/Models/BehaviourStrategies/BenevolentStrategyTest.java
+++ test/Models/BehaviourStrategies/BenevolentStrategyTest.java
@@ -13,20 +13,39 @@ import org.junit.jupiter.api.TestInstance;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/**
+ * Unit tests for the {@link BenevolentStrategy} class, which represents
+ * the benevolent behavior strategy for a player in the game. The tests
+ * cover the creation of deploy and advance orders based on the specific
+ * strategy rules.
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BenevolentStrategyTest {
-
+    /**
+     * The benevolent strategy instance under test.
+     */
     private BenevolentStrategy benevolentStrategy;
+    /**
+     * The player instance used in the tests.
+     */
     private Player player;
+    /**
+     * The WarMap instance used in the tests.
+     */
     private WarMap warMap;
+    /**
+     * A map representing countries used in the tests.
+     */
 
     private Map<Integer, Country> countries;
     /**
-     * Initializing the player instance before each test.
+     * The GameEngine instance used in the tests.
      */
     private GameEngine gameEngine;
-
+    /**
+     * Sets up the initial state for each test, including creating a player,
+     * a benevolent strategy instance, and initializing countries and the game engine.
+     */
     @Before
     public void setUp() {
         player = new Player("John Doe");
@@ -35,7 +54,10 @@ public class BenevolentStrategyTest {
         gameEngine = GameEngine.getInstance();
         warMap = new WarMap();
     }
-
+    /**
+     * Tests the creation of a deploy order command for the benevolent strategy.
+     * Verifies that the order is created correctly based on certain conditions.
+     */
     @Test
    public void testCreateDeployOrderCommand() {
 
@@ -63,7 +85,11 @@ public class BenevolentStrategyTest {
 
         assertEquals(1, player.get_playerOrder().size());
     }
-
+    /**
+     * Tests the creation of an advance order command for the benevolent strategy
+     * when there are neighboring countries. Ensures that the order is created
+     * correctly based on certain conditions.
+     */
     @Test
     public void testCreateAdvanceOrderCommandWithNeighbouringCountries() {
 
@@ -98,7 +124,11 @@ public class BenevolentStrategyTest {
 
         assertEquals(1, player.get_playerOrder().size());
     }
-
+    /**
+     * Tests the creation of an advance order command for the benevolent strategy
+     * when there are no neighboring countries. Ensures that the order is not created
+     * under certain conditions.
+     */
     @Test
     public void testCreateAdvanceOrderCommandWithoutNeighbouringCountries() {
 
