79d936c09940017bf826b6167a20d71069d27ed8
LeilaMsv <leilmousavi377@gmail.com>
2023-11-29 00:31:47 -0800

    CheaterStrategy issue_order implemented

--- src/Models/BehaviourStrategies/CheaterStrategy.java
+++ src/Models/BehaviourStrategies/CheaterStrategy.java
@@ -1,6 +1,9 @@
 package Models.BehaviourStrategies;
 
 import Models.Player;
+import Models.Country;
+
+import java.util.List;
 
 public class CheaterStrategy extends BehaviourStrategyBase {
     public CheaterStrategy(Player p_player){
@@ -8,6 +11,34 @@ public class CheaterStrategy extends BehaviourStrategyBase {
     }
     @Override
     public void issue_order() {
+        for (Country country : d_player.get_playerCountries()) {
+            // Conquer all immediate neighboring enemy countries
+            for (Country neighbor : country.getNeighbouringCountries().values()) {
+                if (neighbor.getD_ownerPlayer() != d_player) {
+                    // Conquer the enemy country
+                    neighbor.setD_ownerPlayer(d_player);
+                    // Double the armies on the conquered country
+                    int currentArmies = neighbor.get_numOfArmies();
+                    neighbor.set_numOfArmies(currentArmies * 2);
+                }
+            }
 
+            // Double the armies on countries with enemy neighbors
+            if (hasEnemyNeighbors(country)) {
+                int currentArmies = country.get_numOfArmies();
+                country.set_numOfArmies(currentArmies * 2);
+            }
+        }
+    }
+
+    /**
+     * Check if a country has enemy neighbors
+     *
+     * @param country The country to check
+     * @return True if the country has enemy neighbors, false otherwise
+     */
+    private boolean hasEnemyNeighbors(Country country) {
+
+        return false;
     }
 }
