464135e5ebde695c2a912e3030f45b32d5f84e04
LeilaMsv <leilmousavi377@gmail.com>
2023-11-29 01:04:25 -0800

    CheaterStrategyTest setup added

--- test/Models/BehaviourStrategies/CheaterStrategyTest.java
+++ test/Models/BehaviourStrategies/CheaterStrategyTest.java
@@ -1,4 +1,20 @@
 package Models.BehaviourStrategies;
+import Models.Player;
+import Models.Country;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CheaterStrategyTest {
+
+    private Player player;
+    private CheaterStrategy cheaterStrategy;
+
+    @Before
+    public void setup() {
+        player = new Player("TestPlayer");
+        cheaterStrategy = new CheaterStrategy(player);
+    }
+
 }
